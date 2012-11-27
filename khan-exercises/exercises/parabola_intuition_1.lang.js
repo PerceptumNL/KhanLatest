28185e21b1cc9d654bb274c54d24b9ddd7656e01
Eveline <eveline@perceptum.nl>
2012-11-27 12:15:46 +0100

    Update khan-exercises/exercises/parabola_intuition_1.lang.js

--- khan-exercises/exercises/parabola_intuition_1.lang.js
+++ khan-exercises/exercises/parabola_intuition_1.lang.js
@@ -1,6 +1,6 @@
 ({
 	"nl" : {
-		"question1"	: 'Adjust the leading coefficient and vertex coordinates to make the blue parabola match the orange parabola.</p>'+
-				'<p>How do these numbers affect the shape and position of a parabola?'
+		"question1"	: 'Verander de leidende coëfficiënt en de coördinaten van de top zodat de blauwe parabool precies samenvalt met de oranje parabool.</p>'+
+				'<p>Wat is de invloed van de getallen op de vorm en de plaats van een parabool?'
 		}
 })

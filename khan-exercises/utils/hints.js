4bd27026923b4111960cf970f6a49c374b4d7355
Ronald Buts <ronald@perceptum.nl>
2012-11-28 11:48:14 +0100

    Hintbutton

--- khan-exercises/utils/hints.js
+++ khan-exercises/utils/hints.js
@@ -26,7 +26,7 @@ $(Khan).bind("newProblem", function() {
     $("#hint")
         .removeClass("green")
         .addClass("orange")
-        .val("geef mij een hint")
+        .val("Geef mij een hint")
         .data("buttonText", false)
         .stop(true /* clear */, true /* jump */)
         .appendTo("#get-hint-button-container");

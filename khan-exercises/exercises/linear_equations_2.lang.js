bb552e3148217ada0b1ec894e5a61d290bedfcdc
Eveline <eveline@perceptum.nl>
2013-01-03 11:47:40 +0100

    Update khan-exercises/exercises/linear_equations_2.lang.js

--- khan-exercises/exercises/linear_equations_2.lang.js
+++ khan-exercises/exercises/linear_equations_2.lang.js
@@ -1,9 +1,9 @@
 ({
 	"nl" : {
-		"question1"	: 'Solve for <code>x</code>:',
+		"question1"	: 'Los op voor <code>x</code>:',
 		
-		"hint1"		: '<var>B > 0 ? "Subtract" : "Add"</var> <code><var>abs(B)</var></code> <var>B > 0 ? "from" : "to"</var> both sides:',
-		"hint2"		: 'Divide both sides by <code><var>A</var></code>.',
-		"hint3"		: 'Simplify.'
+		"hint1"		: '<var>B > 0 ? "Trek" : "Tel"</var> <code><var>abs(B)</var></code> <var>B > 0 ? "af van" : "op bij"</var> beide kanten:',
+		"hint2"		: 'Deel beide kanten door <code><var>A</var></code>.',
+		"hint3"		: 'Vereenvoudig.'
 		}
 })

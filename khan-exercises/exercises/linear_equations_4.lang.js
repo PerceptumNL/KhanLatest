62775db8df25c8b0da0e17775bc259331ad649b7
Eveline <eveline@perceptum.nl>
2013-01-03 11:08:38 +0100

    Update khan-exercises/exercises/linear_equations_4.lang.js

--- khan-exercises/exercises/linear_equations_4.lang.js
+++ khan-exercises/exercises/linear_equations_4.lang.js
@@ -1,14 +1,14 @@
 ({
 	"nl" : {
-		"question1"	: 'Solve for <code>x</code>:',
-		"question2"	: 'Solve for <code>x</code>:',
+		"question1"	: 'Los <code>x</code> op:',
+		"question2"	: 'Los <code>x</code> op:',
 		
-		"hint1"		: 'Multiply both sides by <code><var>expr(["*", C, "x"])</var> + <var>D</var></code>:',
-		"hint2"		: 'Now <var>A * D < 0 ? "add" : "subtract"</var> <code><var>abs( A * D )</var></code> <var>A * D < 0 ? "to" : "from"</var> both sides:',
-		"hint3"		: 'Divide by <code><var>A * C</var></code>:',
-		"hint4"		: 'Multiply both sides by <code><var>expr(["*", C, "x"])</var> + <var>D</var></code>:',
-		"hint5"		: 'Now <var>E * C < 0 ? "add" : "subtract"</var> <code><var>abs( E * C )</var>x</code> <var>E * C < 0 ? "to" : "from"</var> both sides:',
-		"hint6"		: 'Now <var>B < 0 ? "add" : "subtract"</var> <code><var>abs( B )</var></code>:',
-		"hint7"		: 'Divide by <code><var>A - E * C</var></code>:'
+		"hint1"		: 'Vermenigvuldig beide kanten met <code><var>expr(["*", C, "x"])</var> + <var>D</var></code>:',
+		"hint2"		: '<var>A * D < 0 ? "Tel nu" : "Trek nu"</var> <code><var>abs( A * D )</var></code> <var>A * D < 0 ? "op bij" : "af van"</var> both sides:',
+		"hint3"		: 'Deel door<code><var>A * C</var></code>:',
+		"hint4"		: 'Vermenigvuldig beide kanten met <code><var>expr(["*", C, "x"])</var> + <var>D</var></code>:',
+		"hint5"		: '<var>E * C < 0 ? "Tel nu" : "Trek nu"</var> <code><var>abs( E * C )</var>x</code> <var>E * C < 0 ? "op bij" : "af van"</var> both sides:',
+		"hint6"		: '<var>B < 0 ? "Tel nu" : "Trek nu"</var> <code><var>abs( B )</var></code>:',
+		"hint7"		: 'Deel door<code><var>A - E * C</var></code>:'
 		}
 })

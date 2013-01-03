8b0adc5e0c6acb43015fae80b22ecf3c851b3cfc
Eveline <eveline@perceptum.nl>
2013-01-03 13:51:56 +0100

    Update khan-exercises/exercises/point_slope_form.lang.js

--- khan-exercises/exercises/point_slope_form.lang.js
+++ khan-exercises/exercises/point_slope_form.lang.js
@@ -1,24 +1,24 @@
 ({
 	"nl" : {
-		"question1"	: 'Using the following values, create an equation in point slope form.  In other words, given the values below, for a formula that looks like <code>(y - y_{1}) = m(x - x_{1})</code>, what are the values of <code>x_{1}</code>, <code>y_{1}</code>, and <code>m</code>?',
-		"question2"	: 'you may enter integers, reduced fractions or exact decimals for each term',
-		"question3"	: 'pay attention to the sign of each number you enter to be sure the entire equation is correct',
-		"question4"	: ' A line passes through both <code>(\color{#b22222}{<var>X1</var>}, \color{#b22222}{<var>Y1</var>})</code> and <code>(\color{#4169E1}{<var>X2</var>}, \color{#4169E1}{<var>Y2</var>})</code>. Express the equation of the line in point slope form.',
-		"question5"	: 'you may enter integers, reduced fractions or exact decimals for each term',
-		"question6"	: 'pay attention to the sign of each number you enter to be sure the entire equation is correct',
-		"question7"	: ' The slope of a line is <code>\color{#68228B}{<var>fractionReduce(Y1 - Y2, X1 - X2)</var>}</code> and the y-intercept is <code>\color{#4169E1}{<var>Y1</var>}</code>. Express the equation of the line in point slope form.',
-		"question8"	: 'you may enter integers, reduced fractions, or rounded decimals for each term',
-		"question9"	: 'pay attention to the sign of each number you enter to be sure the entire equation is correct',
+		"question1"	: 'Maak, uitgaande van de volgende waarden, een vergelijking in punt-helling vorm. In andere woorden, bepaal aan de hand van de waarden die hier onder zijn weergegeven een formule die er als volgt uit ziet: <code>(y - y_{1}) = m(x - x_{1})</code>, wat zijn de waardes van <code>x_{1}</code>, <code>y_{1}</code>, en <code>m</code>?',
+		"question2"	: 'je mag hele getallen, vereenvoudigde breuken of exacte kommagetallen invullen in elke term',
+		"question3"	: 'let goed op het teken van elk getal wat je invult zodat je zeker weet dat de hele vergelijking is correct.',
+		"question4"	: 'Een lijn gaat door <code>(\color{#b22222}{<var>X1</var>}, \color{#b22222}{<var>Y1</var>})</code> en <code>(\color{#4169E1}{<var>X2</var>}, \color{#4169E1}{<var>Y2</var>})</code>. Geef de vergelijking van de lijn weer in een punt-helling vorm.',
+		"question5"	: 'je mag hele getallen, vereenvoudigde breuken of exacte kommagetallen invullen in elke term',
+		"question6"	: 'let goed op het teken van elk getal wat je invult zodat je zeker weet dat de hele vergelijking is correct',
+		"question7"	: ' De helling van een lijn is <code>\color{#68228B}{<var>fractionReduce(Y1 - Y2, X1 - X2)</var>}</code> en de y-startwaarde is <code>\color{#4169E1}{<var>Y1</var>}</code>. Geef de vergelijking van de lijn weer in een punt-helling vorm. ',
+		"question8"	: 'je mag hele getallen, vereenvoudigde breuken of exacte kommagetallen invullen in elke term',
+		"question9"	: 'let goed op het teken van elk getal wat je invult zodat je zeker weet dat de hele vergelijking is correct',
 		
-		"hint1"		: '<code>f(x)</code> is just a fancy term for <code>y</code>.  So one point is <code>(\color{#b22222}{<var>X1</var>}, \color{#b22222}{<var>Y1</var>})</code>.',
-		"hint2"		: 'The formula to find the slope is: <code>m = (y_{1} - y_{2}) / (x_{1} - x_{2})</code>.',
-		"hint3"		: 'So, by plugging in the numbers, we get <code>\displaystyle {} \frac{\color{#b22222}{<var>Y1</var>} - \color{#4169E1}{<var>Y2</var>}}{\color{#b22222}{<var>X1</var>} - \color{#4169E1}{<var>X2</var>}}</code> =<code>\color{#68228B}{<var>fractionReduce(Y1 - Y2, X1 - X2)</var>}</code>',
-		"hint4"		: 'Select one of the points to substitute for <code>x_{1}</code> and <code>y_{1}</code> in the point slope formula.  The solution is then either:',
-		"hint5"		: 'The formula to find the slope is: <code>m = (y_{1} - y_{2}) / (x_{1} - x_{2})</code>.',
-		"hint6"		: 'So, by plugging in the numbers, we get <code>\displaystyle {} \frac{\color{#b22222}{<var>Y1</var>} - \color{#4169E1}{<var>Y2</var>}}{\color{#b22222}{<var>X1</var>} - \color{#4169E1}{<var>X2</var>}} = \color{#68228B}{<var>fractionReduce(Y1 - Y2, X1 - X2)</var>}</code>',
-		"hint7"		: 'Select one of the points to substitute for <code>x_{1}</code> and <code>y_{1}</code> in the point slope formula.  The solution then becomes either:',
-		"hint8"		: 'The y-intercept is the value of <code>y</code> when <code>x = 0</code>, so it defines a point you can use:<code>\quad(\color{#b22222}{<var>X1</var>}, \color{#b22222}{<var>Y1</var>})</code>.',
-		"hint9"		: 'An equation in point slope form looks like: <code>(y - y_{1}) = m(x - x_{1})</code> ',
-		"hint10"	: ' Thus, the solution in point slope form can be written as:<br /><code>(y - \color{#b22222}{<var>Y1</var>}) = \color{#68228B}{<var>fractionReduce(Y1 - Y2, X1 - X2)</var>}(x - \color{#b22222}{<var>X1</var>})</code>'
+		"hint1"		: '<code>f(x)</code> is een ingewikkelde weergave van <code>y</code>.  Dus een punt is <code>(\color{#b22222}{<var>X1</var>}, \color{#b22222}{<var>Y1</var>})</code>.',
+		"hint2"		: 'De formule om de helling te vinden is: <code>m = (y_{1} - y_{2}) / (x_{1} - x_{2})</code>.',
+		"hint3"		: 'Dus door de nummers in te vullen krijgen we <code>\displaystyle {} \frac{\color{#b22222}{<var>Y1</var>} - \color{#4169E1}{<var>Y2</var>}}{\color{#b22222}{<var>X1</var>} - \color{#4169E1}{<var>X2</var>}}</code> =<code>\color{#68228B}{<var>fractionReduce(Y1 - Y2, X1 - X2)</var>}</code>',
+		"hint4"		: 'Kies 1 van de punten <code>x_{1}</code> en <code>y_{1}</code> om in te vullen in de punt-helling formule.  De oplossing is dan een van deze:',
+		"hint5"		: 'De formule om de helling te vinden is: <code>m = (y_{1} - y_{2}) / (x_{1} - x_{2})</code>.',
+		"hint6"		: 'Dus door de nummers in te vullen krijgen we <code>\displaystyle {} \frac{\color{#b22222}{<var>Y1</var>} - \color{#4169E1}{<var>Y2</var>}}{\color{#b22222}{<var>X1</var>} - \color{#4169E1}{<var>X2</var>}} = \color{#68228B}{<var>fractionReduce(Y1 - Y2, X1 - X2)</var>}</code>',
+		"hint7"		: 'Kies 1 van de punten <code>x_{1}</code> en <code>y_{1}</code> om in te vullen in de punt-helling formule.  De oplossing is dan een van deze:',
+		"hint8"		: 'De y-startwaarde is de waarde van <code>y</code> als <code>x = 0</code>, dus het definieert een te gebruiken punt:<code>\quad(\color{#b22222}{<var>X1</var>}, \color{#b22222}{<var>Y1</var>})</code>.',
+		"hint9"		: 'Een vergelijking in punt-helling vorm ziet er als volgt uit: <code>(y - y_{1}) = m(x - x_{1})</code> ',
+		"hint10"	: ' Dus de oplossing in punt-helling vorm kan geschreven worden als: <br /><code>(y - \color{#b22222}{<var>Y1</var>}) = \color{#68228B}{<var>fractionReduce(Y1 - Y2, X1 - X2)</var>}(x - \color{#b22222}{<var>X1</var>})</code>'
 		}
 })

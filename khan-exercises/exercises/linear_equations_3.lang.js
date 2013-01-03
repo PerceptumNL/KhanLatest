03166b967bd84aab80f5a06b0728e9f20d721e50
Eveline <eveline@perceptum.nl>
2013-01-03 11:51:52 +0100

    Update khan-exercises/exercises/linear_equations_3.lang.js

--- khan-exercises/exercises/linear_equations_3.lang.js
+++ khan-exercises/exercises/linear_equations_3.lang.js
@@ -1,14 +1,14 @@
 ({
 	"nl" : {
-		"question1"	: 'Solve for <code>x</code>:',
+		"question1"	: 'Los op voor <code>x</code>:',
 		
-		"hint1"		: 'Subtract <code><var>C</var>x</code> from both sides:',
-		"hint2"		: '<var>B > 0 ? "Subtract" : "Add"</var> <code><var>abs(B)</var></code> <var>B > 0 ? "from" : "to"</var> both sides:',
-		"hint3"		: 'Divide both sides by <code><var>A - C</var></code>.',
-		"hint4"		: 'Simplify.',
-		"hint5"		: 'Subtract <code><var>A</var>x</code> from both sides:',
-		"hint6"		: '<var>D > 0 ? "Subtract" : "Add"</var> <code><var>abs(D)</var></code> <var>D > 0 ? "from" : "to"</var> both sides:',
-		"hint7"		: 'Divide both sides by <code><var>C - A</var></code>.',
-		"hint8"		: 'Simplify.'
+		"hint1"		: 'Trek <code><var>C</var>x</code> af van beide kanten:',
+		"hint2"		: '<var>B > 0 ? "trek" : "Tel"</var> <code><var>abs(B)</var></code> <var>B > 0 ? "af van" : "op bij"</var> beide kanten:',
+		"hint3"		: 'Deel beide kanten door <code><var>A - C</var></code>.',
+		"hint4"		: 'Vereenvoudig.',
+		"hint5"		: 'Trek <code><var>A</var>x</code> af van beide kanten:',
+		"hint6"		: '<var>D > 0 ? "trek" : "Tel"</var> <code><var>abs(D)</var></code> <var>D > 0 ? "af van" : "op bij"</var> beide kanten:',
+		"hint7"		: 'Deel beide kanten door <code><var>C - A</var></code>.',
+		"hint8"		: 'Vereenvoudig.'
 		}
 })

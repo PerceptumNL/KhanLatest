8a7fada2f2836e484fcebb5796475db70cd51109
Maarten <maarten@perceptum.nl>
2013-01-30 17:59:47 +0100

    made home page a little bit prettier, and also a little bit more broken

--- javascript/homepage-package/homepage.js
+++ javascript/homepage-package/homepage.js
@@ -1,30 +1,43 @@
 var Homepage = {
 
-    init: function() {
+    init:function () {
         VideoControls.initThumbnails();
         Homepage.initWaypoints();
         Homepage.loadData();
+        Homepage.prepareList();
     },
 
-    initPlaceholder: function(youtube_id) {
+    prepareList:function () {
+        $('#subject-list').find('li:has(ul)').click(function (event) {
+            if (this == event.target) {
+                $(this).toggleClass('expanded');
+                $(this).children('ul').toggle('medium');
+            }
+            return false;
+        }).addClass('collapsed').children('ul').hide();
+    },
+
+    initPlaceholder:function (youtube_id) {
         var jelPlaceholder = $("#main-video-placeholder");
-        VideoControls.initPlaceholder(jelPlaceholder, {"youtubeId": youtube_id});
+        VideoControls.initPlaceholder(jelPlaceholder, {"youtubeId":youtube_id});
     },
 
-    initWaypoints: function() {
+    initWaypoints:function () {
 
         // Waypoint behavior not supported in IE7-
         if ($.browser.msie && parseInt($.browser.version, 10) < 8) return;
 
         $.waypoints.settings.scrollThrottle = 50;
 
-        $("#browse").waypoint(function(event, direction) {
+        $("#browse").waypoint(function (event, direction) {
 
             var jel = $(this);
             var jelFixed = $("#browse-fixed");
             var jelTop = $("#back-to-top");
 
-            jelTop.click(function() {Homepage.waypointTop(jel, jelFixed, jelTop);});
+            jelTop.click(function () {
+                Homepage.waypointTop(jel, jelFixed, jelTop);
+            });
 
             if (direction == "down")
                 Homepage.waypointVideos(jel, jelFixed, jelTop);
@@ -33,12 +46,12 @@ var Homepage = {
         });
     },
 
-    waypointTop: function(jel, jelFixed, jelTop) {
+    waypointTop:function (jel, jelFixed, jelTop) {
         jelFixed.css("display", "none");
         if (!$.browser.msie) jelTop.css("display", "none");
     },
 
-    waypointVideos: function(jel, jelFixed, jelTop) {
+    waypointVideos:function (jel, jelFixed, jelTop) {
         jelFixed.css("width", jel.width()).css("display", "block");
         if (!$.browser.msie) jelTop.css("display", "block");
         if (CSSMenus.active_menu) CSSMenus.active_menu.removeClass("css-menu-js-hover");
@@ -47,7 +60,7 @@ var Homepage = {
     /**
      * Loads the contents of the topic data.
      */
-    loadData: function() {
+    loadData:function () {
         var cacheToken = window.Homepage_cacheToken;
         // Currently, this is being A/B tested with the conventional rendering
         // method (where everything is rendered on the server). If there is
@@ -57,25 +70,25 @@ var Homepage = {
             return;
         }
         $.ajax({
-            type: "GET",
-            url: "/api/v1/topics/library/compact",
-            dataType: "jsonp",
+            type:"GET",
+            url:"/api/v1/topics/library/compact",
+            dataType:"jsonp",
 
             // The cacheToken is supplied by the host page to indicate when the library
             // was updated. Since it's fully cacheable, the browser can pull from the
             // local client cache if it has the data already.
-            data: {"v": cacheToken},
+            data:{"v":cacheToken},
 
             // Explicitly specify the callback, since jQuery will otherwise put in
             // a randomly named callback and break caching.
-            jsonpCallback: "__dataCb",
-            success: function(data) {
+            jsonpCallback:"__dataCb",
+            success:function (data) {
                 Homepage.renderLibraryContent(data);
             },
-            error: function() {
+            error:function () {
                 KAConsole.log("Fout bij het laden van initiele data.");
             },
-            cache: true
+            cache:true
         });
     },
 
@@ -87,9 +100,9 @@ var Homepage = {
      * library_content_template.html, and changes must increment
      * library._layout_version.
      */
-    renderLibraryContent: function(topics) {
+    renderLibraryContent:function(topics) {
         var template = Templates.get("homepage.videolist");
-        $.each(topics, function(i, topic) {
+        $.each(topics, function (i, topic) {
             var items = topic["children"];
             var itemsPerCol = Math.ceil(items.length / 3);
             var colHeight = itemsPerCol * 18;
@@ -111,5 +124,7 @@ var Homepage = {
     }
 };
 
-$(function() {Homepage.init();});
+$(function () {
+    Homepage.init();
+});
 

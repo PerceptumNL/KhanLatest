9dcaefb423e788b3ee4f17c9c31e69f24c85e801
Eveline <eveline@perceptum.nl>
2013-03-18 15:58:44 +0100

    changes (no fixes per se)

--- khan-exercises/exercises/alternate_interior_angles.lang.js
+++ khan-exercises/exercises/alternate_interior_angles.lang.js
@@ -1,7 +1,7 @@
 ({
 	"nl" : {
 		"exercise1" : 'De twee horizontale lijnen lopen evenwijdig, en een derde lijn snijdt ze zoals beneden wordt getoond.',
-		"exercise2" : 'Als we weten dat de <span class="hint_blue">blauwe hoek <var>MEASURE</var> graden is</span>, hoe groot is dan <span class="hint_orange">de oranje hoek</span>?',
+		"exercise2" : 'Als we weten dat de <span class="hint_blue"> blauwe hoek <var>MEASURE</var> graden is</span>, hoe groot is dan <span class="hint_orange">de oranje hoek</span>?',
 		"hint1"	: 	'Tegenoverliggende hoeken zijn gelijk, dus de roze hoek is ook <var>MEASURE</var> graden.',
 		"hint2"	:	'De roze en oranje hoeken zijn overeenkomstig, deze zijn dus gelijk.',
 		"hint3"	:	'De oranje hoek is ook <var>MEASURE</var> graden.',

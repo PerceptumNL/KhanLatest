86b201cbb082f2c395508a062d0a132d7d088861
nicholaschris <nicholaschristopherpringle@gmail.com>
2013-05-03 17:52:21 +0200

    fixed new_page

--- new_page.py
+++ new_page.py
@@ -1,5 +1,7 @@
 import request_handler
+import user_utils
 class ViewNewPage(request_handler.RequestHandler):
+    @user_utils.open_access
     def get(self):
         some_value = 100
         template_values = {'some_value': some_value}

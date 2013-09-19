5e8980b2d1a9deacd949485843d0a3af686100f9
Elise <koster.elise@gmail.com>
2013-09-19 15:28:14 +0200

    Fixed language issues

--- khan-exercises/exercises/linear_equations_4.lang.js
+++ khan-exercises/exercises/linear_equations_4.lang.js
@@ -1,14 +1,14 @@
 ({
 	"nl" : {
-		"question1"	: 'Los <code>x</code> op:',
-		"question2"	: 'Los <code>x</code> op:',
+		"question1"	: 'Los op voor <code>x</code>:',
+		"question2"	: 'Los op voor <code>x</code>:',
 		
 		"hint1"		: 'Vermenigvuldig beide kanten met <code><var>expr(["*", C, "x"])</var> + <var>D</var></code>:',
-		"hint2"		: '<var>A * D < 0 ? "Tel nu" : "Trek nu"</var> <code><var>abs( A * D )</var></code> <var>A * D < 0 ? "op bij" : "af van"</var> both sides:',
+		"hint2"		: '<var>A * D < 0 ? "Tel nu" : "Trek nu"</var> <code><var>abs( A * D )</var></code> <var>A * D < 0 ? "op bij" : "af van"</var> beide kanten',
 		"hint3"		: 'Deel door<code><var>A * C</var></code>:',
 		"hint4"		: 'Vermenigvuldig beide kanten met <code><var>expr(["*", C, "x"])</var> + <var>D</var></code>:',
-		"hint5"		: '<var>E * C < 0 ? "Tel nu" : "Trek nu"</var> <code><var>abs( E * C )</var>x</code> <var>E * C < 0 ? "op bij" : "af van"</var> both sides:',
-		"hint6"		: '<var>B < 0 ? "Tel nu" : "Trek nu"</var> <code><var>abs( B )</var></code>:',
-		"hint7"		: 'Deel door<code><var>A - E * C</var></code>:'
+		"hint5"		: '<var>E * C < 0 ? "Tel nu" : "Trek nu"</var> <code><var>abs( E * C )</var>x</code> <var>E * C < 0 ? "op bij" : "af van"</var> beide kanten:',
+		"hint6"		: '<var>B < 0 ? "Tel nu " : "Trek nu hier "</var> <code><var>abs( B )</var></code> <var> B < 0 ? " op aan beide kanten. " : " af van beide kanten. "',
+		"hint7"		: 'Deel door <code><var>A - E * C </var></code>:'
 		}
 })

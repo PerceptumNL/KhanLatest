2a745b3a0ed3c4a3760246e064739853caeb6fea
Sergio <sergio@perceptum.nl>
2013-09-20 16:14:22 +0200

    Update exercises: breuken_vergelijken_2.html decimals_on_the_number_line_2.html division_0.5.html linear_equations_1.html linear_equations_4.html number_line_3.html place_number_in_boxes.html reading_bar_charts_2.html tafel_van_12510.html telling_time_interactive.html

--- khan-exercises/exercises/decimals_on_the_number_line_2.lang.js
+++ khan-exercises/exercises/decimals_on_the_number_line_2.lang.js
@@ -6,7 +6,7 @@
 		"hint1"		: 'Omdat <code>\\color{orange}{<var>SOLUTION</var>}</code> <span data-if="SOLUTION > 0">positief</span><span data-else>negatief</span> is, moet de oranje stip <span data-if="SOLUTION > 0">rechts</span><span data-else>links</span> van de 0 staan.',
 		"hint2"		: 'Scheid de decimaal van het gehele getal:',
 		"hint3"		: 'We weten nu dus dat <code>\\color{orange}{<var>SOLUTION</var>}</code> zich op de getallenlijn tussen de <code>\\color{orange}{<var>SOLUTION_WHOLE</var>}</code> en de <code>\\color{orange}{<var>SOLUTION_WHOLE + (SOLUTION > 0 ? 1 : -1)</var>}</code> bevindt.',
-		"hint4"		: 'De oranje stip moet naar <code>\\color{orange}{<var>SOLUTION</var>}</code> rechts van de 0.',
+		"hint4"		: 'De oranje stip moet naar <code>\\color{orange}{<var>SOLUTION</var>}</code>.',
 		"hint5"		: 'Het oranje getal toont waar <code>\\color{orange}{<var>SOLUTION</var>}</code> op de getallenlijn is.'
 		}
 })
